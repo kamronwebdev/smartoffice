d9b63e92414dda314a015edc22ddaeb69ca022e1
kamronwebdev <kamronbektirkashev05@gmail.com>
2026-04-07 12:38:22 +0500

    Auto start DB

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,16 +1,28 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
-import os
+﻿import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+print('Auto-migrating and collecting static files before running WSGI server...')
+try:
+    execute_from_command_line(['manage.py', 'migrate'])
+    print('Migrate tugallandi.')
+except Exception as e:
+    print('Migrate xatolik berdi:', e)
+
+try:
+    execute_from_command_line(['manage.py', 'collectstatic', '--no-input'])
+    print('Collectstatic tugallandi.')
+except Exception as e:
+    print('Collectstatic xatolik:', e)
+
+try:
+    os.system('python create_superuser.py')
+    print('Admin tekshiruvi tugadi.')
+except Exception as e:
+    pass
 
 application = get_wsgi_application()
